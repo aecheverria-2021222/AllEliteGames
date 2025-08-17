d9337c461ed8958b39e7ece4e90750855508780d
aecheverria-2021222 <aecheverria-2021222@kinal.edu.gt>
2025-08-17 01:11:55 -0600

    refactor: Modificar modelo Empleado.java de AllEliteGames

---
+++ AllEliteGames/src/java/config/Conexion.java
@@ -0,0 +1,19 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+   Connection conexion;
+   
+   public Connection Conexion(){
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+//           conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBAllEliteGames?useSSL=false","root","rodri1507*.wx0_8");
+           conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBAllEliteGames?useSSL=false","quintom","admin");
+       }catch(Exception e){
+            e.printStackTrace();
+       }
+       return conexion;
+    }
+}
---
+++ AllEliteGames/src/java/modelo/EmpleadoDAO.java
@@ -0,0 +1,35 @@
+package modelo;
+
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class EmpleadoDAO {
+
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int resp;
+
+    public Empleado Validar(String usuario, String contrasena) {
+    Empleado empleado = new Empleado();
+    String sql = "select * from Empleado where usuario = ? and contrasena = ?";
+    try {
+        con = cn.Conexion();
+        ps = con.prepareStatement(sql);
+        ps.setString(1, usuario);
+        ps.setString(2, contrasena);
+        rs = ps.executeQuery(); 
+        while (rs.next()) {
+            empleado.setCodigoEmpleado(rs.getInt("CodigoEmpleado"));
+            empleado.setUsuario(rs.getString("usuario"));
+            empleado.setContrasena(rs.getString("contrasena")); 
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return empleado;
+}
+}
