5d270d43e4642f0452a3b29ca197f07dc3f58b28
jgonzales-2024075 <jgonzales-2024075@kinal.edu.gt>
2025-08-19 08:43:50 -0600

    add: Agregar modelo Proveedores en proyecto AllEliteGames

---
+++ AllEliteGames/src/java/modelo/Empleado.java
@@ -0,0 +1,96 @@
+package modelo;
+
+public class Empleado {
+    private int codigoEmpleado; 
+    private String DPIEmpleado ;
+    private String nombresEmpleado; 
+    private String telefonoEmpleado;
+    private String correoEmpleado;
+    private String estado;
+    private String usuario;
+    private String contrasena;
+
+    public Empleado() {
+    }
+
+    public Empleado(int codigoEmpleado, String DPIEmpleado, String nombresEmpleado,String telefonoEmpleado, String correoEmpleado, String estado, String usuario, String contrasena) {
+        this.codigoEmpleado = codigoEmpleado;
+        this.DPIEmpleado = DPIEmpleado;
+        this.nombresEmpleado = nombresEmpleado;
+        this.telefonoEmpleado = telefonoEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.estado = estado;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public int getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(int codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    public String getDPIEmpleado() {
+        return DPIEmpleado;
+    }
+
+    public void setDPIEmpleado(String DPIEmpleado) {
+        this.DPIEmpleado = DPIEmpleado;
+    }
+
+    public String getNombresEmpleado() {
+        return nombresEmpleado;
+    }
+
+    public void setNombresEmpleado(String nombresEmpleado) {
+        this.nombresEmpleado = nombresEmpleado;
+    }
+
+    public String getTelefonoEmpleado() {
+        return telefonoEmpleado;
+    }
+
+    public void setTelefonoEmpleado(String telefonoEmpleado) {
+        this.telefonoEmpleado = telefonoEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+
+  
+    
+}
+
---
+++ AllEliteGames/src/java/modelo/Genero.java
@@ -0,0 +1,62 @@
+package modelo;
+
+public class Genero {
+    private int codigoGenero;
+    private String genero;
+    private String edadRecomendable;
+    private String popularidad;
+    private String publicoObjetivo;
+
+    public Genero() {
+    }
+
+    public Genero(int codigoGenero, String genero, String edadRecomendable, String popularidad, String publicoObjetivo) {
+        this.codigoGenero = codigoGenero;
+        this.genero = genero;
+        this.edadRecomendable = edadRecomendable;
+        this.popularidad = popularidad;
+        this.publicoObjetivo = publicoObjetivo;
+    }
+
+    public int getCodigoGenero() {
+        return codigoGenero;
+    }
+
+    public void setCodigoGenero(int codigoGenero) {
+        this.codigoGenero = codigoGenero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEdadRecomendable() {
+        return edadRecomendable;
+    }
+
+    public void setEdadRecomendable(String edadRecomendable) {
+        this.edadRecomendable = edadRecomendable;
+    }
+
+    public String getPopularidad() {
+        return popularidad;
+    }
+
+    public void setPopularidad(String popularidad) {
+        this.popularidad = popularidad;
+    }
+
+    public String getPublicoObjetivo() {
+        return publicoObjetivo;
+    }
+
+    public void setPublicoObjetivo(String publicoObjetivo) {
+        this.publicoObjetivo = publicoObjetivo;
+    }
+    
+    
+}
