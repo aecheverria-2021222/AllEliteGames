e89cb65ea75cfb8511a5f335f9ff074e176665fe
ljuarez-2021238 <ljuarez-2021238@kinal.edu.gt>
2025-08-19 10:14:07 -0600

    add: Agregar modelo Videojuego al proyecto AllEliteGames

---
+++ AllEliteGames/src/java/modelo/Consola.java
@@ -0,0 +1,83 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Consola {
+    private int codigoConsola;
+    private String nombreConsola;
+    private Double precioConsola;
+    private int stockConsola;
+    private String marca;
+    private Date fechaLanzamiento;
+    private int codigoProveedor;
+
+    public Consola() {
+    }
+
+    public Consola(int codigoConsola, String nombreConsola, Double precioConsola, int stockConsola, String marca, Date fechaLanzamiento, int codigoProveedor) {
+        this.codigoConsola = codigoConsola;
+        this.nombreConsola = nombreConsola;
+        this.precioConsola = precioConsola;
+        this.stockConsola = stockConsola;
+        this.marca = marca;
+        this.fechaLanzamiento = fechaLanzamiento;
+        this.codigoProveedor = codigoProveedor;
+    }
+
+    public int getCodigoConsola() {
+        return codigoConsola;
+    }
+
+    public void setCodigoConsola(int codigoConsola) {
+        this.codigoConsola = codigoConsola;
+    }
+
+    public String getNombreConsola() {
+        return nombreConsola;
+    }
+
+    public void setNombreConsola(String nombreConsola) {
+        this.nombreConsola = nombreConsola;
+    }
+
+    public Double getPrecioConsola() {
+        return precioConsola;
+    }
+
+    public void setPrecioConsola(Double precioConsola) {
+        this.precioConsola = precioConsola;
+    }
+
+    public int getStockConsola() {
+        return stockConsola;
+    }
+
+    public void setStockConsola(int stockConsola) {
+        this.stockConsola = stockConsola;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Date getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(Date fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public int getCodigoProveedor() {
+        return codigoProveedor;
+    }
+
+    public void setCodigoProveedor(int codigoProveedor) {
+        this.codigoProveedor = codigoProveedor;
+    }
+    
+}
